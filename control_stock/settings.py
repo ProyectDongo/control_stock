163bc1d81631bec628bea84a2498c161b98fb482
ProyectDongo <Dongo.proyectos@gmail.com>
2025-12-15 21:26:25 -0300

    idioma

--- control_stock/settings.py
+++ control_stock/settings.py
@@ -130,8 +130,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
 
+LANGUAGE_CODE = "es-cl"
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
